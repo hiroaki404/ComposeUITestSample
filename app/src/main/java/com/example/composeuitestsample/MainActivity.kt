ea3dfd90101c040b6864c5245a5d1282121ab8cd
hiroaki404 <nosho404@gmail.com>
2024-11-23 09:18:04 +0900

    add sample variation

--- app/src/main/java/com/example/composeuitestsample/MainActivity.kt
+++ app/src/main/java/com/example/composeuitestsample/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.composeuitestsample.ui.SampleButtonScreen
 import com.example.composeuitestsample.ui.SampleScreen
 import com.example.composeuitestsample.ui.theme.ComposeUITestSampleTheme
 
@@ -25,6 +26,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingPreview() {
     ComposeUITestSampleTheme {
-        SampleScreen()
+        SampleButtonScreen()
     }
 }
--- app/src/test/java/com/example/composeuitestsample/SampleScreenTest.kt
+++ app/src/test/java/com/example/composeuitestsample/SampleScreenTest.kt
@@ -1,15 +1,19 @@
 package com.example.composeuitestsample
 
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.isRoot
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onRoot
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.printToString
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.example.composeuitestsample.ui.SampleScreen
+import com.example.composeuitestsample.ui.SampleButtonScreen
+import com.example.composeuitestsample.ui.SampleScreenContent
+import com.example.composeuitestsample.ui.SampleWithDialogScreen
+import com.example.composeuitestsample.ui.SampleWithSheetScreen
 import com.github.takahirom.roborazzi.RobolectricDeviceQualifiers
-import com.github.takahirom.roborazzi.captureRoboImage
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -24,9 +28,23 @@ class SampleScreenTest {
 
     @Config(qualifiers = RobolectricDeviceQualifiers.Pixel7)
     @Test
+    fun verify_sample_tree() {
+        composeTestRule.setContent {
+            SampleScreenContent()
+        }
+        println(composeTestRule.onRoot().printToString()) // userUnmergedTree = false by default
+
+        println(composeTestRule.onRoot(useUnmergedTree = true).printToString())
+
+        composeTestRule.onNode(hasText("sample"))
+            .assertIsDisplayed()
+    }
+
+    @Config(qualifiers = RobolectricDeviceQualifiers.Pixel7)
+    @Test
     fun verify_merged_tree() {
         composeTestRule.setContent {
-            SampleScreen()
+            SampleButtonScreen()
         }
         println(composeTestRule.onRoot().printToString()) // userUnmergedTree = false by default
         println(composeTestRule.onRoot(useUnmergedTree = true).printToString())
@@ -37,13 +55,48 @@ class SampleScreenTest {
         // ok
         composeTestRule.onNode(hasText("sample"), useUnmergedTree = false)
             .performClick()
-        // NG
+
+        // NG, deleted child testTag in mergedTree
+//        composeTestRule.onNode(hasTestTag("sample"))
+//            .assertIsDisplayed()
 //        composeTestRule.onNode(hasTestTag("sample"))
 //            .performClick()
+
+        // ok
         composeTestRule.onNode(hasTestTag("sample"), useUnmergedTree = true)
             .performClick()
+    }
 
+    @Config(qualifiers = RobolectricDeviceQualifiers.Pixel7)
+    @Test
+    fun verify_with_dialog_tree() {
+        composeTestRule.setContent {
+            SampleWithDialogScreen()
+        }
+        composeTestRule.onNode(hasText("show dialog"))
+            .performClick()
+        // ng
+//        println(
+//            composeTestRule.onRoot()
+//                .printToString()
+//        )
+
+        // get dialog tree
+        println(
+            composeTestRule.onAllNodes(isRoot())[1]
+                .printToString()
+        )
+    }
+
+    @Config(qualifiers = RobolectricDeviceQualifiers.Pixel7)
+    @Test
+    fun verify_with_sheet_tree() {
+        composeTestRule.setContent {
+            SampleWithSheetScreen()
+        }
+        composeTestRule.onNode(hasText("show sheet"))
+            .performClick()
         composeTestRule.onRoot()
-            .captureRoboImage()
+            .also { println(it.printToString()) }
     }
 }
